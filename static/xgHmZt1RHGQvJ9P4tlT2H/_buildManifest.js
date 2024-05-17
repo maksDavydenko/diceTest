666c3ff0aa6f46ab3e98d591fb08049c6eda8327
maksym davydenko <maksim.davydenko.work@gmail.com>
2024-05-17 18:25:48 +0300

    Updates

---
+++ static/xgHmZt1RHGQvJ9P4tlT2H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/264-784399359f64a3ff.js","static/chunks/pages/index-fc670b83d99e0747.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
